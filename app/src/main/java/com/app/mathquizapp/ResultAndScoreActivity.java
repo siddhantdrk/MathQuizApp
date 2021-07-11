d254c77ad529a218ceb52414250645ec6e9361a5
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-07-12 01:16:13 +0530

    Initial commit

---
+++ app/src/main/java/com/app/mathquizapp/ResultAndScoreActivity.java
@@ -0,0 +1,18 @@
+package com.app.mathquizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ResultAndScoreActivity extends AppCompatActivity {
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_and_score);
+    }
+}
